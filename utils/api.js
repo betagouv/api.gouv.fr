21b6c8edafeb45101d28a1591993fee1951a952b
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-12-13 13:29:14 +0100

    Simplify fetch helpers

--- utils/api.js
+++ utils/api.js
@@ -1,36 +1,19 @@
 import "isomorphic-unfetch"; // eslint-disable-line import/no-unassigned-import
 import getConfig from "next/config";
 
-import HttpError from "./http-error";
-
 const {
   publicRuntimeConfig: { API_URL }
 } = getConfig();
 
-export async function _fetch(url) {
-  const options = {
-    mode: "cors",
-    method: "GET"
-  };
-
-  const response = await fetch(url, options);
-  const contentType = response.headers.get("content-type");
-
-  if (!response.ok) {
-    throw new HttpError(response);
-  }
-
-  if (response.ok && contentType && contentType.includes("application/json")) {
-    return response.json();
-  }
-
-  throw new Error("Une erreur est survenue");
+export async function fetchJson(url) {
+  const response = await fetch(url);
+  return response.json();
 }
 
 export const getAPI = async id => {
-  return _fetch(API_URL + `/apis/${id}.json`);
+  return fetchJson(`${API_URL}/apis/${id}.json`);
 };
 
 export const getAllAPIs = async () => {
-  return _fetch(API_URL + "/api.json");
+  return fetchJson(`${API_URL}/api.json`);
 };
--- utils/http-error.js
+++
@@ -1,9 +0,0 @@
-export default class HttpError extends Error {
-  constructor(response) {
-    super(response.statusText);
-
-    this.name = "HttpError";
-    this.code = response.status;
-    this.url = response.url;
-  }
-}
