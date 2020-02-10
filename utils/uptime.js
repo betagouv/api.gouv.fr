3803d6b29e9285d941a921310b0f62e74f74fe97
xavierJp <xajouppe@gmail.com>
2020-02-10 14:33:52 +0100

    merge utils and lib

---
+++ utils/uptime.js
@@ -0,0 +1,26 @@
+import colors from "../styles/colors";
+
+export const getUptimeState = uptime => {
+  const { red, orange, green } = colors;
+  let color = red; // Red
+
+  if (uptime >= 98) {
+    color = green; // Green
+  } else if (uptime >= 90) {
+    color = orange; // Orange
+  }
+
+  return color;
+};
+
+export const roundUptime = (digits = 2) => {
+  return uptime => {
+    if (typeof uptime === Number) {
+      return uptime.toFixed(digits);
+    } else {
+      // should fail if uptime is neither a string nor a number
+      const upTimeAsNum = parseInt(uptime, 10);
+      return upTimeAsNum.toFixed(digits);
+    }
+  };
+};
