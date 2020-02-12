cb4b2b26aa36e92e43a139b735e7fdd584e6da2f
xavierJp <xajouppe@gmail.com>
2020-02-12 18:51:53 +0100

    update utils and improve font loading

---
+++ const/links.js
@@ -0,0 +1,7 @@
+export default {
+  MAIL_REQUEST_API:
+    'mailto:contact@api.gouv.fr?subject=Demande%20d%27une%20nouvelle%20API',
+  ADD_API:
+    'https://github.com/betagouv/api.gouv.fr/blob/master/CONTRIBUTING.md#ajouter-une-api',
+  SIGNUP: 'https://signup.api.gouv.fr',
+};
--- utils/index.js
+++ utils/index.js
@@ -1,19 +1,18 @@
 /**
  * All or part of the element is visible
  */
-export const isElementVisible = el => {
+export const isElementVisible = (el, offsetTop = 0) => {
   const rect = el.getBoundingClientRect();
   const windowHeight =
     window.innerHeight || document.documentElement.clientHeight;
-
-  return !(rect.top > windowHeight || rect.bottom < 0);
+  return !(rect.top > windowHeight - offsetTop || rect.bottom < 0 + offsetTop);
 };
 
 /**
  * Get hash from window location
  */
 export const getWindowHash = () => {
-  if (typeof window === "undefined" || !window.location.hash) {
+  if (typeof window === 'undefined' || !window.location.hash) {
     return null;
   }
   return window.location.hash.substr(1);
