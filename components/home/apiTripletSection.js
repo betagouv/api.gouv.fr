79ec848a55b87541d3ec5267d9d14917d9ddd7a0
xavierJp <xajouppe@gmail.com>
2020-03-02 16:58:27 +0100

    add randomness to homepage api selection

--- components/home/apiTripletSection.js
+++ components/home/apiTripletSection.js
@@ -1,43 +1,47 @@
 import React from 'react';
+import { shuffle } from 'lodash';
 
 import ApiCard from '../searchApis/apiCard';
 import constants from '../../constants';
 
 import { ButtonLink } from '../../uiComponents';
 
-const ApiTripletSection = ({ apiList }) => (
-  <section id="apis">
-    <div className="content-container">
-      <h3>Découvrez des APIs du service public&nbsp;:</h3>
-      <div className="ui three stackable cards">
-        {apiList
-          .sort((a, b) => (a.visits_2019 > b.visits_2019 ? -1 : 1))
-          .slice(0, 3)
-          .map(api => (
+const ApiTripletSection = ({ apiList }) => {
+  const selectApis = shuffle(
+    apiList
+      .sort((a, b) => (a.visits_2019 > b.visits_2019 ? -1 : 1))
+      .slice(0, 15)
+  ).slice(0, 3);
+  return (
+    <section id="apis">
+      <div className="content-container">
+        <h3>Découvrez des APIs du service public&nbsp;:</h3>
+        <div className="ui three stackable cards">
+          {selectApis.map(api => (
             <ApiCard key={api.title} {...api} />
           ))}
+        </div>
       </div>
-    </div>
-    <div className="layout-center">
-      <ButtonLink href="rechercher-api" large alt>
-        Voir toutes les APIs ☞
-      </ButtonLink>
-    </div>
-    <style jsx>{`
-      section#apis {
-        background: ${constants.colors.backgroundBlueGradient};
-        padding: 50px 0;
-      }
+      <div className="layout-center">
+        <ButtonLink href="rechercher-api" large alt>
+          Voir toutes les APIs ☞
+        </ButtonLink>
+      </div>
+      <style jsx>{`
+        section#apis {
+          background-color: ${constants.colors.darkBlue};
+          padding: 50px 0;
+        }
 
-      section#apis h3 {
-        color: #fff;
-        font-weight: 400;
-      }
-      section > div:last-child {
-        padding: 40px 0 0;
-      }
-    `}</style>
-  </section>
-);
-
+        section#apis h3 {
+          color: #fff;
+          font-weight: 400;
+        }
+        section > div:last-child {
+          padding: 40px 0 0;
+        }
+      `}</style>
+    </section>
+  );
+};
 export default ApiTripletSection;
