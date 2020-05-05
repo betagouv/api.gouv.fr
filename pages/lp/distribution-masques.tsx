cb8a99afc4338b850496721489c7e6991f90ac56
xavierJp <xajouppe@gmail.com>
2020-05-05 13:49:52 +0200

    add qui sommes nous section

--- pages/lp/distribution-masques.tsx
+++ pages/lp/distribution-masques.tsx
@@ -13,22 +13,20 @@ const Masks: React.FC = () => (
   >
     <div className="hero-section text-container">
       <h1>Gagnez du temps et protégez vos concitoyens</h1>
-      <h2>
-        Simplifiez la distribution de masques dans votre commune grace à une API
-      </h2>
+      <h2>Simplifiez la distribution de masques dans votre commune</h2>
       <ButtonLink
         href="https://signup.api.gouv.fr/api-particulier"
         onClick={logLPCTA1}
       >
-        Je veux créer un service de distribution de masques dans ma collectivité
+        Je veux créer un service de distribution de masques
       </ButtonLink>
     </div>
     <div className="question-section section">
       <div className="text-container">
         <p>
-          En intégrant API particulier à votre portail citoyen, il suffit à vos
-          concitoyens de donner leur numero fiscal et leur numéro d'allocataire
-          pour que votre équipe accéde à :
+          En connectant le portail de votre ville à l’API Particulier, il suffit
+          à vos concitoyens de donner leur numero fiscal et leur numéro
+          d'allocataire pour que votre équipe puisse accéder à :
         </p>
         <ul>
           <li>
@@ -36,8 +34,8 @@ const Masks: React.FC = () => (
             nombre de personnes à charge
           </li>
           <li>
-            l'adresse ainsi que la composition de son foyer familial (adulte et
-            enfant) connue par la CAF
+            l'adresse ainsi que la composition du foyer familial (adulte et
+            enfant) pour les allocataires CAF
           </li>
         </ul>
         <p>
@@ -61,7 +59,7 @@ const Masks: React.FC = () => (
         <div>
           Vous êtes prêt à connecter votre ville ? Voici les étapes à suivre :
           <ul>
-            <li>Demandez accès à API particulier</li>
+            <li>Demandez accès à l’API particulier</li>
             <li>Connectez votre portail citoyen</li>
             <li>Organisez la distribution de masque</li>
           </ul>
@@ -70,7 +68,7 @@ const Masks: React.FC = () => (
               href="https://signup.api.gouv.fr/api-particulier"
               onClick={logLPCTA2}
             >
-              Je connecte ma ville à API particulier
+              Je connecte ma ville à l’API Particulier
             </ButtonLink>
           </div>
         </div>
@@ -78,16 +76,27 @@ const Masks: React.FC = () => (
     </div>
 
     <div className="qui-sommes-nous section">
-      <div className="text-container">
-        <h3 className="text-center">Qui sommes-nous ?</h3>
-        <p>Ici on raconte qui nous sommes</p>
+      <div className="text-container text-center">
+        <h3>Qui sommes-nous ?</h3>
+        <p>
+          La DINUM accompagne les ministères dans leur transformation numérique,
+          conseille le gouvernement et développe des services et ressources
+          partagées comme le réseau interministériel de l’État, FranceConnect,
+          data.gouv.fr ou api.gouv.fr.
+        </p>
+        <p>
+          Dans le cadre de la crise sanitaire relative au COVID-19, la DINUM se
+          mobilise afin de mettre à disposition des territoires ses outils et
+          son expertise et faciliter ainsi la mise en oeuvre de démarches en
+          ligne à destination des usagers.
+        </p>
       </div>
     </div>
     <div className="section">
       <div className="text-container">
         <h3 className="text-center">
           Vous avez une question ? <br />
-          Vous avez besoin d’aide pour connecter un service citoyen a une API ?
+          Vous avez besoin d’aide pour connecter un service citoyen à une API ?
         </h3>
         <div className="layout-center">
           <ButtonLink
