201887c58354d952615051b2550650ed60288bf2
Xavier Jouppe <xajouppe@gmail.com>
2020-07-08 13:47:03 +0200

    add loader in access page

--- pages/les-api/[slug]/demande-acces.tsx
+++ pages/les-api/[slug]/demande-acces.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { GetStaticProps, GetStaticPaths } from 'next';
 import ReactMarkdown from 'react-markdown';
 
