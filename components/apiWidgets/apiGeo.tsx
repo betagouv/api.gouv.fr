a4dc91133fad85c360bbd0b37059fac6092342e1
Xavier Jouppe <xajouppe@gmail.com>
2020-07-13 17:03:02 +0200

    create API widget and update search bar

---
+++ components/apiWidgets/apiGeo.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+
+import Loader from '../../uiComponents/loader';
+import SearchBar from '../../uiComponents/searchBar';
+import { result } from 'lodash';
+import constants from '../../constants';
+
+const GeoWidget: React.FC<{}> = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [results, setResults] = useState([]);
+
+  const search = (searchTerm: string) => {
+    if (!searchTerm) {
+      setResults([]);
+    }
+
+    setIsLoading(true);
+    fetch(
+      `https://geo.api.gouv.fr/communes?nom=${encodeURI(
+        searchTerm
+      )}&fields=departement&boost=population&limit=5`
+    )
+      .then(response => response.json())
+      .then(data =>
+        window.setTimeout(() => {
+          setResults(
+            data.map(
+              (res: any) =>
+                `${res.nom}, ${res.departement.nom} (${res.departement.code})`
+            )
+          );
+          setIsLoading(false);
+        }, 400)
+      )
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+  return (
+    <div className="widget-wrapper layout-center">
+      <div className="widget-label">Un exemple d’API ouverte : l’API Geo</div>
+      <div>
+        L’
+        <a href="/les-api/api-geo">API Geo</a> permet notamment d'effectuer une
+        recherche par nom des communes de France. Voila comment cela se passe :
+      </div>
+      <ol>
+        <li>Vous tapez le nom d’une commune</li>
+        <li>Nous envoyons ce nom à l’API Geo</li>
+        <li>L’API géo nous renvoie la liste des communes qui correspondent</li>
+        <li>Nous affichons les résultats</li>
+      </ol>
+      <div className="interactive-component">
+        <SearchBar
+          onSearch={search}
+          placeholder="Tapez le nom de votre commune"
+          label="Recherchez une commune française"
+          debounceRate={200}
+          results={results}
+          isLoading={isLoading}
+        />
+      </div>
+      <style jsx>{`
+        .widget-wrapper {
+          background-color: ${constants.colors.lightBlue};
+          flex-direction: column;
+          padding: 50px;
+          border-radius: 10px;
+          position: relative;
+          margin: 40px auto;
+        }
+
+        .widget-label {
+          position: absolute;
+          margin: 0;
+          top: -15px;
+          font-weight: bold;
+          background-color: #fff;
+          border-bottom-left-radius: 10px;
+          border-bottom-right-radius: 10px;
+          padding: 3px 10px;
+        }
+
+        p {
+          text-align: center;
+        }
+
+        .interactive-component {
+          min-width: 60%;
+          background-color: #fff;
+          box-shadow: 0 20px 30px 0px rgba(0, 0, 0, 0.2);
+          padding: 20px 40px;
+          border-radius: 5px;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default GeoWidget;
--- uiComponents/loader/index.tsx
+++ uiComponents/loader/index.tsx
@@ -1,62 +1,69 @@
 import React from 'react';
 import constants from '../../constants';
 
-const Loader: React.FC<{}> = () => (
-  <>
-    <div className="loader-container layout-center">
-      <div className="loader">
-        <span></span>
-        <span></span>
+interface IProps {
+  small?: boolean;
+}
+
+const Loader: React.FC<IProps> = ({ small = false }) => {
+  const size = small ? 20 : 50;
+  return (
+    <>
+      <div className="loader-container layout-center">
+        <div className="loader">
+          <span></span>
+          <span></span>
+        </div>
       </div>
-    </div>
-    <style jsx>{`
-      .loader-container {
-        width: 100%;
-      }
-      .loader {
-        margin: auto;
-        width: 50px;
-        height: 50px;
-        display: inline-block;
-        padding: 0px;
-        text-align: left;
-      }
-      .loader span {
-        position: absolute;
-        display: inline-block;
-        width: 50px;
-        height: 50px;
-        border-radius: 100%;
-        background: ${constants.colors.vividBlue};
-        -webkit-animation: loader 0.8s linear infinite;
-        animation: loader 0.8s linear infinite;
-      }
-      .loader span:last-child {
-        animation-delay: -0.4s;
-        -webkit-animation-delay: -0.4s;
-      }
-      @keyframes loader {
-        0% {
-          transform: scale(0, 0);
-          opacity: 0.8;
+      <style jsx>{`
+        .loader-container {
+          width: 100%;
         }
-        100% {
-          transform: scale(1, 1);
-          opacity: 0;
+        .loader {
+          margin: auto;
+          width: ${size}px;
+          height: ${size}px;
+          display: inline-block;
+          padding: 0px;
+          text-align: left;
         }
-      }
-      @-webkit-keyframes loader {
-        0% {
-          -webkit-transform: scale(0, 0);
-          opacity: 0.8;
+        .loader span {
+          position: absolute;
+          display: inline-block;
+          width: ${size}px;
+          height: ${size}px;
+          border-radius: 100%;
+          background: ${constants.colors.vividBlue};
+          -webkit-animation: loader 0.8s linear infinite;
+          animation: loader 0.8s linear infinite;
         }
-        100% {
-          -webkit-transform: scale(1, 1);
-          opacity: 0;
+        .loader span:last-child {
+          animation-delay: -0.4s;
+          -webkit-animation-delay: -0.4s;
         }
-      }
-    `}</style>
-  </>
-);
+        @keyframes loader {
+          0% {
+            transform: scale(0, 0);
+            opacity: 0.8;
+          }
+          100% {
+            transform: scale(1, 1);
+            opacity: 0;
+          }
+        }
+        @-webkit-keyframes loader {
+          0% {
+            -webkit-transform: scale(0, 0);
+            opacity: 0.8;
+          }
+          100% {
+            -webkit-transform: scale(1, 1);
+            opacity: 0;
+          }
+        }
+      `}</style>
+    </>
+  );
+};
 
 export default Loader;
