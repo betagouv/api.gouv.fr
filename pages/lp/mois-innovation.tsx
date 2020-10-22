2b9b0d2920f60bec68d6f494f1fdcb71d2e648fa
Victor Gross <vict.gross@gmail.com>
2020-10-22 18:03:27 +0200

    Update mois-innovation.tsx
    Précision sur la nature des "données publiques"

--- pages/lp/mois-innovation.tsx
+++ pages/lp/mois-innovation.tsx
@@ -48,6 +48,11 @@ const MoisInno: React.FC = () => (
           <b>vos demandes d'accès à des données publiques</b> et les{' '}
           <b>services que vous souhaitez construire</b> grâce à ces données.
         </p>
+        <p> 
+          Par données publiques nous entendons les documents administratifs produits ou reçus 
+          dans le cadre d'une mission de service public par l'Etat, les collectivités territoriales 
+          ainsi que par les autres personnes de droit public ou de droit privé chargées d'une telle mission.
+        </p>
       </div>
     </div>
     <div className="section">
