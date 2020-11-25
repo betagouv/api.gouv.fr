acb95fcd5b77b9d11e7cf58e2cc356b05f011aaa
Xavier Jouppe <xajouppe@gmail.com>
2020-11-25 14:36:26 +0100

    remove redirection

--- next.config.js
+++ next.config.js
@@ -13,11 +13,6 @@ module.exports = withFonts({
         destination: '/datapass/api',
         permanent: true,
       },
-      {
-        source: '/les-api/loire-atlantique-donnees-ouvertes',
-        destination: '/les-api/api-loire-atlantique-donnees-ouvertes',
-        permanent: true,
-      },
     ];
   },
   webpack(config, options) {
