f105e276b1b9320b2870e57fee0cbf87dcacd416
Xavier Jouppe <xajouppe@gmail.com>
2020-12-14 12:18:22 +0100

    Tech doc consistency

--- components/api/technical-documentation.tsx
+++ components/api/technical-documentation.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { ButtonLink } from '../../uiComponents/button';
 import Emoji from '../../uiComponents/emoji';
-import external from '../../uiComponents/icon/external';
 import SubSection from './subSection';
 
 interface IProps {
@@ -15,34 +14,38 @@ const TechnicalDocumentation: React.FC<IProps> = ({
   swagger_link,
   external_link,
   slug,
-}) => (
-  <SubSection title="Documentation technique">
-    {external_link || swagger_link ? (
-      <>
-        <div>
-          Vous êtes développeur ou architecte ? Partagez la documentation avec
-          votre équipe :
-        </div>
-        <div className="layout-right vertical-margin">
-          {swagger_link && (
-            <ButtonLink href={`/documentation/${slug}`}>
-              <Emoji emoji="👩‍💻" label="code" />
-              Tester l’API
-            </ButtonLink>
-          )}
-        </div>
-        <div className="layout-right vertical-margin">
-          {external_link && (
-            <ButtonLink href={external_link} alt>
-              Accéder à la documentation {external}
-            </ButtonLink>
-          )}
-        </div>
-      </>
-    ) : (
-      <div>Cette API n’a pas de documentation publique.</div>
-    )}
-  </SubSection>
-);
+}) => {
+  const link = swagger_link ? `/documentation/${slug}` : external_link;
+  return (
+    <SubSection title="Documentation technique">
+      {external_link || swagger_link ? (
+        <>
+          <div>
+            Vous êtes développeur ou architecte ? Partagez la documentation avec
+            votre équipe :
+          </div>
+          <div className="layout-right vertical-margin">
+            {swagger_link && (
+              <ButtonLink href={`/documentation/${slug}`}>
+                <Emoji emoji="👩‍💻" label="code" />
+                Tester l’API
+              </ButtonLink>
+            )}
+          </div>
+          <div className="layout-right vertical-margin">
+            {link && (
+              <ButtonLink href={link} alt>
+                <Emoji emoji="📚" label="documentation" />
+                Accéder à la documentation
+              </ButtonLink>
+            )}
+          </div>
+        </>
+      ) : (
+        <div>Cette API n’a pas de documentation publique.</div>
+      )}
+    </SubSection>
+  );
+};
 
 export default TechnicalDocumentation;
