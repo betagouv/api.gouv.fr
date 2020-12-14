15305beb36f6c869c2b64b5dca653398c8ab7d0e
Xavier Jouppe <xajouppe@gmail.com>
2020-12-14 11:01:03 +0100

    add doc button

--- components/api/technical-documentation.tsx
+++ components/api/technical-documentation.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import { ButtonLink } from '../../uiComponents/button';
+import Emoji from '../../uiComponents/emoji';
+import external from '../../uiComponents/icon/external';
 import SubSection from './subSection';
 
 interface IProps {
@@ -13,27 +15,34 @@ const TechnicalDocumentation: React.FC<IProps> = ({
   swagger_link,
   external_link,
   slug,
-}) => {
-  const link = swagger_link ? `/documentation/${slug}` : external_link;
-  return (
-    <SubSection title="Documentation technique">
-      {link ? (
-        <>
-          <div>
-            Vous êtes développeur ou architecte ? Partagez la documentation avec
-            votre équipe :
-          </div>
-          <div className="layout-right vertical-margin">
-            <ButtonLink href={link} alt>
-              Accéder à la documentation
+}) => (
+  <SubSection title="Documentation technique">
+    {external_link || swagger_link ? (
+      <>
+        <div>
+          Vous êtes développeur ou architecte ? Partagez la documentation avec
+          votre équipe :
+        </div>
+        <div className="layout-right vertical-margin">
+          {swagger_link && (
+            <ButtonLink href={`/documentation/${slug}`}>
+              <Emoji emoji="👩‍💻" label="code" />
+              Tester l’API
             </ButtonLink>
-          </div>
-        </>
-      ) : (
-        <div>Cette API n’a pas de documentation publique.</div>
-      )}
-    </SubSection>
-  );
-};
+          )}
+        </div>
+        <div className="layout-right vertical-margin">
+          {external_link && (
+            <ButtonLink href={external_link} alt>
+              Accéder à la documentation {external}
+            </ButtonLink>
+          )}
+        </div>
+      </>
+    ) : (
+      <div>Cette API n’a pas de documentation publique.</div>
+    )}
+  </SubSection>
+);
 
 export default TechnicalDocumentation;
