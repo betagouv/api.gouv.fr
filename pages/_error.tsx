f314a5883fac1c4df24c41189df3f6e017f37c41
Xavier Jouppe <xajouppe@gmail.com>
2021-03-30 09:59:08 +0200

    fix error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -13,29 +13,12 @@ interface IProps {
   message?: string;
 }
 
-class ErrorPage extends React.Component<IProps> {
-  static defaultProps = {
-    code: undefined,
-    message: null,
-  };
-
-  static getStaticProps({ res, err }: any) {
-    const code = res ? res.statusCode : err ? err.statusCode : null;
-
-    return { code };
-  }
-
-  render() {
-    const { code, message } = this.props;
-    const title = `Erreur ${code}`;
-    const msg = message || messages[code];
-
-    return (
-      <Page title="Page d’erreur">
-        <Error title={title} msg={msg} />
-      </Page>
-    );
-  }
-}
+const ErrorPage: React.FC<IProps> = ({ code, message }) => {
+  return (
+    <Page title="Page d’erreur">
+      <Error title={`Erreur ${code}`} msg={message || messages[code]} />
+    </Page>
+  );
+};
 
 export default ErrorPage;
