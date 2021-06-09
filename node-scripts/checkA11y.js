68d9672366e83863b979d105888c889e59b56b9a
Xavier Jouppe <xajouppe@gmail.com>
2021-06-09 10:42:23 +0200

    Update packages

--- node-scripts/checkA11y.js
+++ node-scripts/checkA11y.js
@@ -6,8 +6,7 @@ console.info('=== A11y checker ===');
 fs.readFile('./public/url-list.txt', (err, urls) => {
   if (err) throw err;
   exec(
-    './node_modules/axe-cli/axe-cli ' +
-      // 'http://localhost:3000/les-api/franceconnect --exit --disable scrollable-region-focusable region',
+    'axe ' +
       urls.toString().split('\n').join(' ') +
       ' --stdout --exit --disable scrollable-region-focusable --disable region',
     (err, stdout, stderr) => {
