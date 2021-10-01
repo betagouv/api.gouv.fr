7500627934a35d6afc653082c65be7712c29c3b2
Xavier Jouppe <xajouppe@gmail.com>
2021-10-01 16:33:06 +0200

    remove Cyril and Vic and Add ben and Mat

--- pages/equipe.tsx
+++ pages/equipe.tsx
@@ -60,22 +60,22 @@ const teamData = [
     img: 'Loic.jpg',
   },
   {
-    name: 'Victor Gross',
+    name: 'Benoit Morane',
     title: 'Développement des offres API',
-    linkedin: 'https://fr.linkedin.com/in/victorgross',
-    img: 'Victor.jpg',
+    linkedin: 'https://www.linkedin.com/in/benoit-morane-0b5773a5',
+    img: 'Benoit.jpg',
+  },
+  {
+    name: 'Mathilde Hoang',
+    title: 'Chargée de mission open data',
+    linkedin: 'https://fr.linkedin.com/in/mathilde-hoang-01a72a88',
+    img: 'Benoit.jpg',
   },
   {
     name: 'Alexis Leclerc',
     title: 'API Entreprise',
     linkedin: 'https://www.linkedin.com/in/leclerca/',
     img: 'Alexis.jpg',
-  },
-  {
-    name: 'Cyril Kato',
-    title: 'API Entreprise',
-    linkedin: 'https://www.linkedin.com/in/cyrilllllll',
-    img: 'Cyril.jpg',
   },
   {
     name: 'Dorine Lambinet',
