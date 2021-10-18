5bdc14cd9042c89b1da96d4244c9d1d52989ff4a
Samuelfaure <samuel.faure.dev@gmail.com>
2021-10-18 16:41:34 +0200

    More options for questionTree
    - Replacing strings by JSX.Element for easy manipulation (adding
      <strong>)
    - Added description (optional)

--- components/questionTree/data.js
+++ components/questionTree/data.js
@@ -1,8 +1,9 @@
 const apiEntrepriseOrAssociation = {
-  question: 'Premiere question',
+  question: 'Votre entreprise/association est-elle dîte “chargée d’une mission de service public” ?',
+  description: 'Les SNCF, la Poste sont des exemples d’entreprises chargées d’une mission de service public, tel que définies par la loi.',
   choiceTree: [
       {
-        choice: 'choix 1',
+        choice: <span><strong>Oui</strong>, tel qu’inscrit dans la loi</span>,
         next: {
           question: 'Seconde question',
           choiceTree: [
--- components/questionTree/index.tsx
+++ components/questionTree/index.tsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import { apiEntrepriseQuestionTree } from './data'
 
 interface IQuestionTree {
-  question: string,
+  question: JSX.Element,
+  description?: JSX.Element,
   choiceTree: ChoiceType[]
 }
 
 interface ChoiceType {
-  choice: string,
+  choice: JSX.Element,
   next?: IQuestionTree,
   result?: JSX.Element
 }
@@ -18,6 +19,7 @@ const Question: React.FC<{questionTree: IQuestionTree}> = ({questionTree}) => {
   return (
     <div className='question-tree-wrapper'>
       <p><strong>{questionTree.question}</strong></p>
+      <p>{questionTree.description}</p>
       {
         questionTree.choiceTree.map((choiceType, key) =>
           <button
@@ -25,7 +27,7 @@ const Question: React.FC<{questionTree: IQuestionTree}> = ({questionTree}) => {
             onClick={() => setChoiceType(choiceType)}
             className={choiceType === currentChoiceType ? 'selected' : ''}
           >
-            {choiceType.choice}
+            <p className='choice'>{choiceType.choice}</p>
           </button>
         )
       }
