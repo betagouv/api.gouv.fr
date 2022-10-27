7918fe4e9ff17a7eab2793b0f754b3b7a4b91a47
Nicolas Schweisguth <n.schweisguth@gmail.com>
2022-10-27 15:04:34 +0200

    factorisation texte

--- _data/api/api-particulier.md
+++ _data/api/api-particulier.md
@@ -15,7 +15,10 @@ access_page:
       - Une collectivité ou une administration
     is_eligible: 1
     description: |
+      Conformément aux dispositions de <External href="https://www.legifrance.gouv.fr/affichCodeArticle.do?cidTexte=LEGITEXT000031366350&idArticle=LEGIARTI000031367412&dateTexte=&categorieLien=cid">l'article L114-8</External> du *code des relations entre le public et l'administration*, seules les administrations sont habilitées à échanger entre elles des informations ou données strictement nécessaires pour traiter une démarche.
 
+      Pour obtenir un agrément, vous devez **justifier d'une simplification pour les citoyens**, et vous engager à
+      n'accéder aux données personnelles qu'avec **l'accord explicite** de l'usager.
       <QuestionTree tree='api-particulier' question='apipart'/>
   - who:
       - Un éditeur de logiciel
--- components/questionTree/data/api-particulier/index.ts
+++ components/questionTree/data/api-particulier/index.ts
@@ -15,12 +15,7 @@ const apiParticulierQuestions = {
                 'Mon équipe de développeurs (ou moi-même)',
                 'Autre / Je ne sais pas'
             ],
-            answer: `Conformément aux dispositions de <External href="https://www.legifrance.gouv.fr/affichCodeArticle.do?cidTexte=LEGITEXT000031366350&idArticle=LEGIARTI000031367412&dateTexte=&categorieLien=cid">l'article L114-8</External> du *code des relations entre le public et l'administration*, seules les administrations sont habilitées à échanger entre elles des informations ou données strictement nécessaires pour traiter une démarche.
-            <br/><br/>
-            Pour obtenir un agrément, vous devez **justifier d'une simplification pour les citoyens**, et vous engager à
-            n'accéder aux données personnelles qu'avec **l'accord explicite** de l'usager.
-            <br/><br/>
-            <Button href="https://datapass.api.gouv.fr/api-particulier">Remplir une demande</Button>
+            answer: `<br/><Button href="https://datapass.api.gouv.fr/api-particulier">Remplir une demande</Button>
             `,
             },
     ],
