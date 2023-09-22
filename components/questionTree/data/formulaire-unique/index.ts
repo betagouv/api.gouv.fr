e63d0d3c20c789a2c3b0643fa4055a7b2e489ff7
Pierre-Eric Garcia <garciapierreeric@gmil.com>
2023-09-22 08:35:13 +0200

    Factorize formulaire unique questions creation

--- components/questionTree/data/formulaire-unique/index.ts
+++ components/questionTree/data/formulaire-unique/index.ts
@@ -1,67 +1,23 @@
+const generateFormulaireUniqueQuestion = (apiName: string) => ({
+  question: 'Faites-vous appel à un éditeur de logiciels ?',
+  choiceTree: [
+    {
+      choices: ['Oui'],
+      answer: `Vous êtes éligible et pouvez remplir une demande pour accéder directement à l'environnement de production. 
+        <br/>Avant de soumettre votre demande, vous devrez vous assurer que votre éditeur de logiciels a bien finalisé ses développements en environnement de tests (bac à sable).
+        <br/><br/><Button href="https://datapass.api.gouv.fr/api-${apiName}-unique">Remplir une demande</Button>`,
+    },
+    {
+      choices: ['Non'],
+      answer: `<br/><Button href="https://datapass.api.gouv.fr/api-${apiName}-sandbox">Remplir une demande</Button>`,
+    },
+  ],
+});
+
 const formulaireUniqueQuestions = {
-  r2p: {
-    question: 'Faites-vous appel à un éditeur de logiciels ?',
-    choiceTree: [
-        {
-        choices: [
-            'Oui',
-        ],
-        answer: `Vous êtes éligible et pouvez remplir une demande pour accéder directement à l'environnement de production. 
-        <br/>Avant de soumettre votre demande, vous devrez vous assurer que votre éditeur de logiciels a bien finalisé ses développements en environnement de tests (bac à sable).
-        <br/><br/><Button href="https://datapass.api.gouv.fr/api-r2p-unique">Remplir une demande</Button>
-        `,
-        },
-        {
-            choices: [
-                'Non',
-            ],
-            answer: `<br/><Button href="https://datapass.api.gouv.fr/api-r2p-sandbox">Remplir une demande</Button>
-            `,
-            },
-    ],
-},
-sfip: {
-    question: 'Faites-vous appel à un éditeur de logiciels ?',
-    choiceTree: [
-        {
-        choices: [
-            'Oui',
-        ],
-        answer: `Vous êtes éligible et pouvez remplir une demande pour accéder directement à l'environnement de production. 
-        <br/>Avant de soumettre votre demande, vous devrez vous assurer que votre éditeur de logiciels a bien finalisé ses développements en environnement de tests (bac à sable).
-        <br/><br/><Button href="https://datapass.api.gouv.fr/api-sfip-unique">Remplir une demande</Button>
-        `,
-        },
-        {
-            choices: [
-                'Non',
-            ],
-            answer: `<br/><Button href="https://datapass.api.gouv.fr/api-sfip-sandbox">Remplir une demande</Button>
-            `,
-            },
-    ],
-},
-ficoba: {
-    question: 'Faites-vous appel à un éditeur de logiciels ?',
-    choiceTree: [
-        {
-        choices: [
-            'Oui',
-        ],
-        answer: `Vous êtes éligible et pouvez remplir une demande pour accéder directement à l'environnement de production. 
-        <br/>Avant de soumettre votre demande, vous devrez vous assurer que votre éditeur de logiciels a bien finalisé ses développements en environnement de tests (bac à sable).
-        <br/><br/><Button href="https://datapass.api.gouv.fr/api-ficoba-unique">Remplir une demande</Button>
-        `,
-        },
-        {
-            choices: [
-                'Non',
-            ],
-            answer: `<br/><Button href="https://datapass.api.gouv.fr/api-ficoba-sandbox">Remplir une demande</Button>
-            `,
-            },
-    ],
-},
-    } as { [key: string]: any };
+  r2p: generateFormulaireUniqueQuestion('r2p'),
+  sfip: generateFormulaireUniqueQuestion('sfip'),
+  ficoba: generateFormulaireUniqueQuestion('ficoba'),
+} as { [key: string]: any };
 
 export default formulaireUniqueQuestions;
