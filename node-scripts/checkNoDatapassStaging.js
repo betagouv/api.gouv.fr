faf0993769ddc272539d385f9bc111103e913340
Nicolas Schweisguth <n.schweisguth@gmail.com>
2023-09-22 15:13:15 +0200

    Remplacement URL staging datapass

--- node-scripts/checkNoDatapassStaging.js
+++ node-scripts/checkNoDatapassStaging.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-exec("grep -rnw './_data/' -e 'datapass-staging'", (err, stdout, stderr) => {
+exec("grep -rnw './_data/' -e 'staging.datapass'", (err, stdout, stderr) => {
   if (stdout) {
     console.log(stdout);
     process.exit(1);
