34326399105d974bab5ee4648be621fc649a46e6
Miryad Ali <59559301+Miryad3108@users.noreply.github.com>
2024-01-11 14:12:45 +0100

    Ajout APIdev Millibase

--- components/questionTree/data/api-particulier/eligible.ts
+++ components/questionTree/data/api-particulier/eligible.ts
@@ -29,6 +29,10 @@ export const pathEligible = {
         answer: `<br/><Button href="https://datapass.api.gouv.fr/api-particulier?demarche=agora-plus">Remplir une demande</Button>
         `,
         },
+        'APIdev (Millibase)',
+        ],
+        answer: `<br/><<Button href="https://datapass.api.gouv.fr/api-particulier?demarche=APIdev-Millibase">Remplir une demande</Button>
+        `,
         {
         choices: [
             'Jdéalise/Cantine de France',
